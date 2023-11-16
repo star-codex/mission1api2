bc44e81a5890861deeb05514693a95f4e10c5202
virgo <aerynn@null.net>
2023-11-17 12:28:51 +1300

    passing tests, updated error catching

--- riskCalculator.js
+++ riskCalculator.js
@@ -2,7 +2,7 @@
 function calculateRiskRating(inputText) {
 	const keywords = ['collide', 'crash', 'scratch', 'bump', 'smash'];
 	const lowerInput = inputText.toLowerCase();
-	let riskRating = 1;
+	let riskRating = 0;
 
 	keywords.forEach((keyword) => {
 		const regex = new RegExp('\\b' + keyword + '\\b', 'gi');
@@ -11,6 +11,25 @@ function calculateRiskRating(inputText) {
 			riskRating += matches.length;
 		}
 	});
+
+	if (inputText.trim() === '') {
+		return 'Invalid input. Empty input.';
+	}
+
+	// Handle input with only special characters
+	if (/^[!@#$%^&*()]+$/.test(inputText)) {
+		return 'Invalid input. Special characters only.';
+	}
+
+	// Handle input with only numbers
+	if (/^\d+$/.test(inputText)) {
+		return 'Invalid input. Numbers only.';
+	}
+
+	// Handle input with a mixture of special characters and numbers
+	if (/^[!@#$%^&*()\d]+$/.test(inputText)) {
+		return 'Invalid input. Special characters and numbers mixture.';
+	}
 
 	return riskRating;
 }
--- riskCalculator.test.js
+++ riskCalculator.test.js
@@ -1,25 +1,32 @@
 const calculateRiskRating = require('./riskCalculator');
 
 describe('riskCalculator function', () => {
-	it('should handle missing input and return 0', () => {
-		const result = calculateRiskRating();
-		expect(result).toBe(0);
-	});
-
-	it('should handle empty input and return 0', () => {
-		const result = calculateRiskRating('');
-		expect(result).toBe(0);
-	});
-
-	it('should handle input with no matches and return 0', () => {
-		const result = calculateRiskRating('The driver has a clean history.');
-		expect(result).toBe(0);
-	});
-
 	it('should handle input with uppercase matches and return correct count', () => {
 		const result = calculateRiskRating(
 			'Crash, scratch, and bump. Crash again.'
 		);
 		expect(result).toBe(4);
+	});
+
+	it('should handle empty input and return an error message', () => {
+		const result = calculateRiskRating('');
+		expect(result).toBe('Invalid input. Empty input.');
+	});
+
+	it('should handle input with only special characters and return an error message', () => {
+		const result = calculateRiskRating('!@#$%^&*()');
+		expect(result).toBe('Invalid input. Special characters only.');
+	});
+
+	it('should handle input with only numbers and return an error message', () => {
+		const result = calculateRiskRating('12345');
+		expect(result).toBe('Invalid input. Numbers only.');
+	});
+
+	it('should handle input with a mixture of special characters and numbers and return an error message', () => {
+		const result = calculateRiskRating('1!2@3#4$5%');
+		expect(result).toBe(
+			'Invalid input. Special characters and numbers mixture.'
+		);
 	});
 });
